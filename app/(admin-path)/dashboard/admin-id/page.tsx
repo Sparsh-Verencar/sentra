51bae98de59e13534954910aba15c754e5d1c0a6
Sparsh Verencar <verencarsparsh@gmail.com>
2026-01-28 17:38:38 +0530

    baburao wanted this

--- app/(admin-path)/dashboard/admin-id/page.tsx
+++ app/(admin-path)/dashboard/admin-id/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AdminId = () => {
   return (
-    <div>AdminId</div>
+    <div className='h-screen w-screen flex items-center justify-center'>
+      <div className='w-60vw h-40vh bg-amber-200 text-2xl text-white'>
+            admin id
+      </div>
+    </div>
   ) 
 }
 
