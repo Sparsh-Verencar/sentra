1cf51cd70597f48fed610ce009b15e0aae21139f
ClimateOP <baburaogaonkar906@gmail.com>
2026-01-31 21:41:00 +0530

    add found functionality

--- convex/lostItem.ts
+++ convex/lostItem.ts
@@ -40,11 +40,30 @@ export const getAllLostPosts = query({
         const item = await ctx.db.get(lost.item_id);
         const student = await ctx.db.get(lost.student_id);
 
+        const found = await ctx.db
+          .query("found")
+          .filter((q) => q.eq(q.field("item_id"), lost.item_id))
+          .first();
+
+        let foundWithStudent: null | {
+          student: any;
+          found_date: string;
+        } = null;
+
+        if (found) {
+          const foundStudent = await ctx.db.get(found.student_id);
+          foundWithStudent = {
+            student: foundStudent!,
+            found_date: found.found_date,
+          };
+        }
+
         return {
           _id: lost._id,
           date: lost.lost_date,
           item,
           student,
+          found: foundWithStudent,
         };
       })
     );
