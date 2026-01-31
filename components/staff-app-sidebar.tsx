42685e81f236910423591d71c81aba93605019c3
Sparsh Verencar <verencarsparsh@gmail.com>
2026-01-31 21:53:21 +0530

    staff dashboard done

---
+++ components/staff-app-sidebar.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  IconChartBar,
+  IconFileAi,
+  IconFileDescription,
+  IconInnerShadowTop,
+} from "@tabler/icons-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { BarChart3, Book, Brain, Camera, FileText, GraduationCapIcon, Megaphone, UniversityIcon, User2 } from "lucide-react"
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Announcements",
+      url: "/staff-dashboard/announcements",
+      icon: Megaphone,
+    },
+    {
+      title: "Complaints",
+      url: "/staff-dashboard/complaints",
+      icon: Book,
+    },
+    {
+      title: "Account",
+      url: "/staff-dashboard/staff-id",
+      icon: User2 ,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: Camera,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: FileText,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: Brain,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function StaffAppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="size-5" />
+                <span className="text-base font-semibold"></span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain as any} />
+      </SidebarContent>
+    </Sidebar>
+  )
+}
